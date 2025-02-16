77df13f2d479a8b2eb58b11901618dd3b7c7d0dd
Svifif <151376009+Svifif@users.noreply.github.com>
2025-02-16 23:31:47 +0300

    Add files via upload

---
+++ Линейная алгебра/ЛА.сем.1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+class Base
+{
+public:
+    unsigned int x;
+};
+
+class SemiGroup
+{
+public:
+    virtual Base sum(Base b) = 0; 
+};
+
+class MySGroup : public SemiGroup, public Base
+{
+public:
+    Base sum(Base b)
+    {
+        Base tmp;
+        tmp.x = x + b.x;
+        return tmp;
+    }
+};
+
+int main()
+{
+    MySGroup  item1, item2;
+    item1.x = 1;
+    item2.x = 2;
+    cout << (item1.sum(item2)).x << endl;
+    SemiGroup* g1 = new MySGroup();
+    SemiGroup* g2 = new MySGroup();
+    return 0;
+}
