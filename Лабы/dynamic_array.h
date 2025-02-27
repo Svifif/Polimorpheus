9187ebb0fd275443bbefac1d27c1efd649341bcf
Svifif <151376009+Svifif@users.noreply.github.com>
2025-02-27 21:21:12 +0300

    Update dynamic_array.h

--- Лабы/dynamic_array.h
+++ Лабы/dynamic_array.h
@@ -19,11 +19,11 @@ typedef struct Dynamic_Array
 Dynamic_Array* create_Array(int assumed_capacity);
 Dynamic_Array* add_value(Dynamic_Array* array, ElementType value, int index);
 void remove_value(Dynamic_Array* array,int index);
-Dynamic_Array* map(Dynamic_Array* array, ElementType(*)(ElementType) func);
+Dynamic_Array* map(Dynamic_Array* array, ElementType(*)(ElementType) func, int index1, int index2);
 Dynamic_Array* where(Dynamic_Array* array, bool(*)(ElementType) predicate);
 Dynamic_Array* concatenate(Dynamic_Array* array1, Dynamic_Array* array2)
 void remove_Array(Dynamic_Array* array);
-//void cpy_array(Dynamic_Array* array, int index1, int index2);
+void cpy_array(Dynamic_Array* array, int index1, int index2);
 
 
 
